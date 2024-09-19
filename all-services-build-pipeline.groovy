d181d08068d89a9b3cb6069c38aa22ffec9b1f14
Vikram Verma <vikram.verma@collaberatact.com>
2024-09-19 16:00:23 +0530

    5 commit

--- all-services-build-pipeline.groovy
+++ all-services-build-pipeline.groovy
@@ -19,7 +19,7 @@ pipeline {
                     echo 'Building eureka-server service...'
                     bat 'mvn clean package'
                     echo 'Deploying the eureka-server'
-                    bat 'start \b java -jar target/eureka-server-0.0.1-SNAPSHOT.jar'
+                    bat 'start java -jar target/eureka-server-0.0.1-SNAPSHOT.jar'
         }
                     dir('apigateway')
         {
